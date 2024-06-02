3d2ee5f0bb339e2bd3e1590871265809c6ac916b
DenZaiyy <denzaiyy@outlook.fr>
2024-06-02 03:08:46 +0200

    adapt manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,30 +3,36 @@ game 'gta5'
 lua54 'yes'
 
 author 'DenZaiyy'
-description ''
+description 'Custom context-menu by DenZaiyy'
 version '1.0.0'
+
+dependencies {
+    'dnz_fed'
+}
 
 -- Shared Scripts
 shared_script {
     '@es_extended/imports.lua',
     '@es_extended/locale.lua',
+    '@oxmysql/lib/MySQL.lua',
     'locales/*.lua',
     'config.lua',
 }
 
 -- Client Scripts
 client_scripts {
-    'client/client.lua',
+    'client/*.lua',
 }
 
 -- Server Scripts
 server_scripts {
-    'server/server.lua',
+    'server/*.lua',
 }
 
 -- NUI
 ui_page 'web/index.html'
 
 files {
-    'web/index.html'
+    'web/index.html',
+    'web/script.js',
 }
